7d08f8b1a877631c4f986463491f06e73f1c2141
mark20435 <mark20435@hotmail.com>
2022-10-28 16:48:35 +0800

    feat: 1. 手動佈點 2. 設定各點的鄰居

---
+++ Assets/Scripts/WP.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WP : MonoBehaviour
+{
+    public List<GameObject> neighbors;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
